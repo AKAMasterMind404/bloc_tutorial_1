652382bb65f07288212635ac3bbe5500cfb32064
Atharv Karbhari <atharvkarbhari123@gmail.com>
2023-04-04 12:35:50 +0530

    removed previous code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flbloc_demo/app/app.dart';
 import 'package:flbloc_demo/my-cubit/number_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flbloc_demo/store/store.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'my-cubit/app_observer.dart';
 
 void main() {
   Bloc.observer = AppObserver();
