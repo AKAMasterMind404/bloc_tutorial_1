e2e5e70bb06df50e9d4e244414f7fb2038f26db3
Atharv Karbhari <atharvkarbhari123@gmail.com>
2023-04-05 13:56:58 +0530

    init commit

---
+++ lib/data/repositories/post_repository.dart
@@ -0,0 +1,20 @@
+import '/data/models/post_model.dart';
+import '/data/repositories/api/api.dart';
+import 'package:dio/dio.dart';
+
+class PostRepository {
+
+  API api = API();
+
+  Future<List<PostModel>> fetchPosts() async {
+    try {
+      Response response = await api.sendRequest.get("/posts");
+      List<dynamic> postMaps = response.data;
+      return postMaps.map((postMap) => PostModel.fromJson(postMap)).toList();
+    }
+    catch(ex) {
+      throw ex;
+    }
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
-import 'package:flbloc_demo/app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flbloc_demo/store/store.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '/logic/cubits/post_cubit/post_cubit.dart';
+import '/presentation/screens/home_screen.dart';
 
-void main() {
-  Bloc.observer = AppObserver();
 
-  runApp(const StoreAppCubit());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => PostCubit(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
+    );
+  }
 }
