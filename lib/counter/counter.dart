d46c8dd8e4ff05bc0ded517fd22ab15f3e0eb175
Atharv Karbhari <atharvkarbhari123@gmail.com>
2023-05-06 14:30:22 +0530

    init

--- lib/counter/counter.dart
+++
@@ -1,3 +0,0 @@
-export 'bloc/counter_bloc.dart';
-export 'bloc/counter_event.dart';
-export 'view/counter_app.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
-import 'package:flbloc_demo/app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flbloc_demo/store/store.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  Bloc.observer = AppObserver();
 
-  runApp(const StoreAppCubit());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
 }
