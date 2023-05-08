af91eff360e5f1afd80a576d9970a4430e4c6c6b
Atharv Karbhari <atharvkarbhari123@gmail.com>
2023-05-08 14:08:14 +0530

    Update DataProvider.dart

--- lib/provider/DataProvider.dart
+++ lib/provider/DataProvider.dart
@@ -5,12 +5,8 @@ class FakeStoreProvider extends ChangeNotifier {
   final Dio _dio = Dio(BaseOptions(baseUrl: 'https://fakestoreapi.com/'));
 
   bool firstCallLoaded = false;
-  bool secondCallLoaded = false;
-  bool thirdCallLoaded = false;
 
   List list1 = [];
-  List list2 = [];
-  List list3 = [];
 
   Future<void> makeFirstCall() async {
     // await Future.delayed(const Duration(seconds: 2));
@@ -20,19 +16,4 @@ class FakeStoreProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> makeSecondCall() async {
-    // await Future.delayed(const Duration(seconds: 2));
-    list2 = (await _dio.get('products')).data as List;
-    print(list2);
-    secondCallLoaded = true;
-    notifyListeners();
-  }
-
-  Future<void> makeThirdCall() async {
-    // await Future.delayed(const Duration(seconds: 2));
-    list3 = (await _dio.get('products')).data as List;
-    print(list3);
-    thirdCallLoaded = true;
-    notifyListeners();
-  }
 }
